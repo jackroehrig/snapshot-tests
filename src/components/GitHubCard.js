a6e8a7bc54d1301c58301ab71741f6eecb5e7cc9
jackroehrig <jack.roe13@gmail.com>
2022-02-01 16:22:45 -0600

    created GitHubCard component

---
+++ src/components/GitHubCard.js
@@ -0,0 +1,17 @@
+import { Card } from "react-bootstrap";
+
+function GitHubCard() {
+    return (
+        <Card>
+            <Card.Img variant='top' src="http://placekitten.com/300/200" />
+            <Card.Body>
+                <Card.Title>Jack Roehrig</Card.Title>
+                <Card.Text>
+                    Hi! My name is Jack, I am from Verona, WI. I played football, hockey, and golf in highschool.
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default GitHubCard
