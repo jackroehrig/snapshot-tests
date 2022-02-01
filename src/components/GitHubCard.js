fa570cfcdd496a12266fcd34b27fe6ad871bb981
jackroehrig <jack.roe13@gmail.com>
2022-02-01 16:29:43 -0600

    created and tested snapshot tests

--- src/components/GitHubCard.js
+++ src/components/GitHubCard.js
@@ -6,6 +6,7 @@ function GitHubCard() {
             <Card.Img variant='top' src="http://placekitten.com/300/200" />
             <Card.Body>
                 <Card.Title>Jack Roehrig</Card.Title>
+                <Card.Subtitle>Born May 17th, 2001</Card.Subtitle>
                 <Card.Text>
                     Hi! My name is Jack, I am from Verona, WI. I played football, hockey, and golf in highschool.
                 </Card.Text>
